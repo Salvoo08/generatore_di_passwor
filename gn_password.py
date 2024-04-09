2709e0c82f12dc162f800345fadff28c0e652ca0
Salvoo08 <165826507+Salvoo08@users.noreply.github.com>
2024-04-09 19:44:44 +0200

    updeit

--- gn_password.py
+++ gn_password.py
@@ -1,1 +1,10 @@
-print("Hello world")
+import random
+caratteri = "+-/*!&$#?=@abcdefghijklnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890"
+
+x = int(input("Inserisci la lungezza della passworld: "))
+
+password = ""
+
+for i in range(x):
+    password = password +  random.choice(caratteri)
+print(password)
